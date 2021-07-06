925a9da2228d6240c82c7ed526be788afa7d6f05
LukaMarsic <73958671+LukaMarsic@users.noreply.github.com>
2021-07-07 01:50:13 +0200

    Uspostava AL klase

--- index.php
+++ index.php
@@ -6,4 +6,27 @@ session_start();
 //echo __DIR__;
 define('BP',__DIR__ . DIRECTORY_SEPARATOR);
 
-echo BP;
+//echo BP;
+
+$putanje=implode(
+    PATH_SEPARATOR,
+    [
+        BP . 'model',
+        BP . 'controller'
+    ]
+);
+
+
+//echo $putanje;
+set_include_path($putanje);
+
+spl_autoload_register(function($klasa){
+    $putanje = explode(PATH_SEPARATOR,get_include_path());
+    foreach($putanje as $p){
+        if (file_exists($p . DIRECTORY_SEPARATOR . $klasa . '.php')){
+            include $p . DIRECTORY_SEPARATOR . $klasa . '.php';
+        }
+    }
+});
+
+App::start();
