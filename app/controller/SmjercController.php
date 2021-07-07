3691e858860ceb45233d717a1ea93cf40d5da43b
LukaMarsic <73958671+LukaMarsic@users.noreply.github.com>
2021-07-07 17:28:07 +0200

    Implementacija rute za smjer

---
+++ app/controller/SmjercController.php
@@ -0,0 +1,16 @@
+<?php
+
+class SmjerController extends AutorizacijaController
+{
+    private $viewDir = 'privatno'
+                        . DIRECTORY_SEPARATOR
+                        . 'smjer'
+                        . DIRECTORY_SEPARATOR;
+
+    public function index()
+    {
+        $this->view->render($this->viewDir . 'index');
+    }
+
+}
+
