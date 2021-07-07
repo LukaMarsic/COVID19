b4c3f59a9f14df5300e4768050a4d6188748f2ca
LukaMarsic <73958671+LukaMarsic@users.noreply.github.com>
2021-07-07 16:41:16 +0200

    Auth Zavrsena - priprema nadzorne ploce

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -17,10 +17,16 @@ class IndexController extends Controller
         $this->view->render('kontakt');
     }
 
-
     public function login()
     {
         $this->loginView('','');
+    }
+
+    public function logout()
+    {
+        unset($_SESSION['autoriziran']);
+        session_destroy();
+        $this->index();
     }
 
     public function autorizacija()
@@ -40,6 +46,18 @@ class IndexController extends Controller
             return;
         }
 
+        //siguran sam da su email i lozinka postavljeni
+        // i sada mogu ići u bazu provjeriti ih - ALI NE DANAS
+        if(!($_POST['email']==='edunova@edunova.hr' && 
+            $_POST['lozinka']==='e') ){
+                $this->loginView($_POST['email'],'Neispravna kombinacija emaila i lozinke');
+                return;
+        }
+
+        $_SESSION['autoriziran']='Edunova Korisnik';
+        $np = new NadzornaplocaController();
+        $np->index();
+
     }
 
     private function loginView($email,$poruka)
@@ -49,8 +67,4 @@ class IndexController extends Controller
             'poruka'=>$poruka
         ]);
     }
-
-
-
-
 }
