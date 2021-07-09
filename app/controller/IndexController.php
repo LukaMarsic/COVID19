fecdaef727dd16fbc68f4291fa43df546419aaac
LukaMarsic <73958671+LukaMarsic@users.noreply.github.com>
2021-07-09 16:19:47 +0200

    Kontrola unesenih podataka za autorizaciju

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -22,4 +22,36 @@ class IndexController extends Controller
     {
         $this->view->render('kontakt');
     }
+
+    public function login()
+    {
+        $this->loginView('','');
+    }
+
+    public function autorizacija()
+    {
+        if(!isset($_POST['email']) || !isset($_POST['lozinka'])){
+            $this->login();
+            return; //short curcuiting
+        }
+
+        if(strlen(trim($_POST['email']))===0){
+            $this->loginView('','Obavezno email');
+            return;
+        }
+
+        if(strlen(trim($_POST['lozinka']))===0){
+            $this->loginView($_POST['email'],'Obavezno lozinka');
+            return;
+        }
+
+    }
+
+    private function loginView($email,$poruka)
+    {
+        $this->view->render('login',[
+            'email'=>$email,
+            'poruka'=>$poruka
+        ]);
+    }
 }
