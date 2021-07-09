9696d666551f3c1e6bbf15647f43a1af7db37418
LukaMarsic <73958671+LukaMarsic@users.noreply.github.com>
2021-07-09 16:23:09 +0200

    Završena autorizacija i priprema Nadzorna ploča

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -26,7 +26,15 @@ class IndexController extends Controller
     public function login()
     {
         $this->loginView('','');
+        
     }
+    public function logout()
+    {
+        unset($_SESSION['autoriziran']);
+        session_destroy();
+        $this->index();
+    }
+
 
     public function autorizacija()
     {
@@ -44,7 +52,16 @@ class IndexController extends Controller
             $this->loginView($_POST['email'],'Obavezno lozinka');
             return;
         }
+        
+        if(!($_POST['email']==='edunova@edunova.hr' && 
+            $_POST['lozinka']==='e') ){
+                $this->loginView($_POST['email'],'Neispravna kombinacija emaila i lozinke');
+                return;
+        }
 
+        $_SESSION['autoriziran']='Edunova Korisnik';
+        $np = new NadzornaplocaController();
+        $np->index();
     }
 
     private function loginView($email,$poruka)
