1faabc3b7cd5dcb35f1e8698a61cead4336698d4
LukaMarsic <73958671+LukaMarsic@users.noreply.github.com>
2021-07-10 02:45:13 +0200

    Zavrsen unos smjera s kontrolama

--- app/controller/SmjerController.php
+++ app/controller/SmjerController.php
@@ -22,7 +22,12 @@ class SmjerController extends AutorizacijaController
             $smjer->cijena=1000;
             $smjer->verificiran='0';
 
-            $this->novoView($smjer,'Popunite sve podatke');
+             // $this->view->render($this->viewDir . 'novo',[
+           //     'smjer'=>$smjer,
+           //     'poruka'=>'Popunite sve podatke'
+           // ]);
+           $this->novoView($smjer,'Popunite sve podatke');
+
             return;
         }
 
@@ -30,16 +35,48 @@ class SmjerController extends AutorizacijaController
         $smjer = (object) $_POST;
 
         if(strlen(trim($smjer->naziv))===0){
-            $this->view->render($this->viewDir . 'novo',[
-                'smjer'=>$smjer,
-                'poruka'=>'Naziv obavezno'
-            ]);
+            // $this->view->render($this->viewDir . 'novo',[
+           //     'smjer'=>$smjer,
+          //      'poruka'=>'Naziv obavezno'
+           // ]);
+           // linija ispod mijenja 4 linije iznad
+            $this->novoView($smjer,'Naziv obavezno');
             return;
         }
 
+        if(strlen(trim($smjer->naziv))>50){
+            $this->novoView($smjer,'Naziv ne može imati više od 50 znakova');
+            return;
+        }
+        
+        if(!is_numeric($smjer->trajanje)
+            || ((int)$smjer->trajanje)<=0){
+            $this->novoView($smjer,'Trajanje mora biti cijeli pozitivni broj');
+            return;
+      }
 
+      $smjer->cijena=str_replace(',','.',$smjer->cijena);
+      if(!is_numeric($smjer->cijena)
+            || ((float)$smjer->cijena)<=0){
+            $this->novoView($smjer,'Cijena mora biti pozitivni broj');
+            return;
+      }
 
+   // npr. svojstvu verificiran ne treba kontrola
+
+      // ovdje sam siguran da je sve OK prije odlaska u bazu
+      Smjer::dodajNovi($smjer);
+      $this->index();
        
     }
+
+    private function novoView($smjer, $poruka)
+    {
+        $this->view->render($this->viewDir . 'novo',[
+            'smjer'=>$smjer,
+            'poruka'=>$poruka
+        ]);
+    }
+
 }
 
